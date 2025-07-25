f2ceeab82ab80de68076f5e10380ef99d4f1475a
dillnaza <99315866+dillnaza@users.noreply.github.com>
2025-07-25 12:08:48 +0500

    Update TelegramNotification.java

--- src/TelegramNotification.java
+++ src/TelegramNotification.java
@@ -19,37 +19,29 @@ public class TelegramNotification  {
     }
 
     public void setAnswer(String text) {
-
         String urlParameters = "chat_id="+getChatId()+"&text="+text;
         byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
-
         try {
             URL url = new URL(urlToken);
             con = (HttpURLConnection) url.openConnection();
-
             con.setDoOutput(true);
             con.setRequestMethod("POST");
             con.setRequestProperty("User-Agent", "Java upread.ru client");
             con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
             try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
                 wr.write(postData);
             }
-
             StringBuilder content;
-
             try (BufferedReader br = new BufferedReader(
                     new InputStreamReader(con.getInputStream()))) {
                 String line;
                 content = new StringBuilder();
-
                 while ((line = br.readLine()) != null) {
                     content.append(line);
                     content.append(System.lineSeparator());
                 }
             }
             System.out.println(content);
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
